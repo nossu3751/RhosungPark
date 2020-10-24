01fb4b49ffd8e860c8c50a8ac5a010f17b35b6d5
Rhosung Park <rhosungpark@gmail.com>
2020-10-23 19:41:53 -0700

    template html works

--- app.js
+++ app.js
@@ -1,9 +1,21 @@
 const express = require("express");
 const app = express();
 
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+
 app.get("/", (req, res) => {
-    res.render("homepage.ejs")
+    res.render("homepage")
 });
+
+app.get("/contact", (req, res) => {
+    res.render("contact")
+});
+
+app.get("/about", (req, res) => {
+    res.render("about")
+});
+
 app.listen(8888, () => {
     console.log("Rhosung Park's website is currently running on port 8888.")
 });
