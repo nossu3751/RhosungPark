52a861357ff99ebc0936e7339f9fdbb5f079826b
Rhosung Park <rhosungpark@gmail.com>
2020-10-23 23:58:20 -0700

    basic routing done

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 const express = require("express");
+const path = require('path');
+
 const app = express();
 
 app.use(express.static("public"));
 app.set("view engine", "ejs");
+
+app.use('/css', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/css')));
+app.use('/js', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/js')));
+app.use('/js', express.static(path.join(__dirname, 'node_modules/jquery/dist')));
 
 app.get("/", (req, res) => {
     res.render("homepage")
