9d760b488b3924364f62f1339da04a6fb64c1736
Rhosung Park <rhosungpark@gmail.com>
2020-11-12 02:13:52 -0800

    resume added, homepage resume link added, email function added

--- app.js
+++ app.js
@@ -1,15 +1,22 @@
 const express = require("express");
 const path = require('path');
 const nodemailer = require('nodemailer');
+const favicon = require('serve-favicon');
 
 const app = express();
+
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.set("view engine", "ejs");
 
+app.use(favicon(path.join(__dirname, 'public', 'assets', 'images', 'rho_favicon.ico')));
 app.use('/css', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/css')));
 app.use('/js', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/js')));
 app.use('/js', express.static(path.join(__dirname, 'node_modules/jquery/dist')));
+
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+
 
 app.get("/", (req, res) => {
     res.render("homepage")
@@ -23,35 +30,46 @@ app.get("/work", (req, res) => {
     res.render("work")
 });
 
-const message = {
-    from: 'elonmusk@tesla.com', // Sender address
-    to: 'to@email.com',         // List of recipients
-    subject: 'Design Your Model S | Tesla', // Subject line
-    text: 'Have the most fun you can in a car. Get your Tesla today!' // Plain text body
-};
+app.post('/', (req, res) => {
 
-const transport = nodemailer.createTransport({
-    host: 'smtp.mailtrap.io',
-    port: 2525,
-    auth: {
-        user: 'f6f6187017979e',
-        pass: '859ecb4adc5c2d'
-    }
-});
-
-// const transport = nodemailer.createTransport({
-//     service:'gmail',
-//     auth:{
-//         user:'rhosungpark@gmail.com'
-//     }
-// });
-
-transport.sendMail(message, function(err, info) {
-    if (err) {
-      console.log(err)
-    } else {
-      console.log(info);
-    }
+    console.log(req);
+    const content = `
+        <ul>
+            <li>Name: ${req.body.name}</li>
+            <li>Email: ${req.body.email}</li>
+            <li>Message: ${req.body.comment}</li>
+        </ul>
+    `;
+    const message = {
+        from: `nodemailer contact ${req.body.email}`, // Sender address
+        to: 'rhosungpark@gmail.com',         // List of recipients
+        subject: 'Email via Rhosung Park Website Contact Form', // Subject line
+        html: content // Plain text body
+    };
+    
+    const transport = nodemailer.createTransport({
+        host: 'smtp.mailtrap.io',
+        port: 2525,
+        auth: {
+            user: 'f6f6187017979e',
+            pass: '859ecb4adc5c2d'
+        }
+    });
+    
+    // const transport = nodemailer.createTransport({
+    //     service:'gmail',
+    //     auth:{
+    //         user:'rhosungpark@gmail.com'
+    //     }
+    // });
+    
+    transport.sendMail(message, function(err, info) {
+        if (err) {
+          console.log(err)
+        } else {
+          console.log(info);
+        }
+    });
 });
 
 app.listen(8888, () => {
--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -3,3 +3,45 @@ $(()=>{
         $(".nav-menu").toggleClass("nav-open");
     });
 });
+
+$(()=>{
+    $("#homepage-image").click(()=>{
+        $('#resume-modal').modal('toggle');
+    })
+});
+
+$(() => {
+    $('#contact-form').on('submit', (event)=>{
+        event.preventDefault();
+        var name = $('#name').val();
+        var email = $('#email').val();
+        var comment = $('#comment').val();
+        
+        console.log(name);
+        console.log(email);
+        console.log(comment);
+        $.ajax({
+            url: '/',
+            method: "POST",
+            contentType: 'application/json',
+            data: JSON.stringify({
+                name,
+                email,
+                comment
+            }),
+            success: (response) => {
+                console.log(name);
+                console.log(response);
+            },
+            fail: (error) => {
+                console.log(error);
+            }
+        });
+
+        $("#name").val('');
+        $("#email").val('');
+        $("#comment").val('');
+
+        $('#contact-submit-modal').modal('toggle');
+    });
+});
